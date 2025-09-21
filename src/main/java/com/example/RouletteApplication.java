67d2800bcdbfb636131094e92365f990222173de
kozakospan2006-dev <kozakos.pan2006@gmail.com>
2025-09-21 16:10:13 +0300

    Initial commit

--- src/main/java/com/example/RouletteApplication.java
+++ src/main/java/com/example/RouletteApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RouletteApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RouletteApplication.class, args);
     }
